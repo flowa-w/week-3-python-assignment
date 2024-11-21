be46871805602152a4f3c416e4d4f170ece85f34
flowa-w <florencewambui86@gmail.com>
2024-11-21 23:32:53 +0300

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,26 @@
+def calculate_discount(price, discount_percentage):
+    if discount_percentage >= 20:
+        discount_amount = price * discount_percentage
+        #final_price = price - discount_amount
+        
+
+
+        input()
+item_price = 100
+discount = 20/100
+discount_amount = 100 * 20/100
+#print(discount_amount)
+
+
+
+discount_amount = 20
+final_price = 100 - 20
+print(final_price)
+
+
+
+    
+
+
+    
+    
